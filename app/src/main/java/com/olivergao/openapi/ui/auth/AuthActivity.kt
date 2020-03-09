86df61574a85850ff82aaf2aab352086449ebef3
Oliver Gao <oliver.peng.gao@gmail.com>
2020-03-10 07:48:51 +1300

    initial commit

---
+++ app/src/main/java/com/olivergao/openapi/ui/auth/AuthActivity.kt
@@ -0,0 +1,13 @@
+package com.olivergao.openapi.ui.auth
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.olivergao.openapi.R
+
+class AuthActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+}
